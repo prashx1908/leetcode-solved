1988c6ae4f31be4105ea17b50491976d844a0127
Prashanth G <77740840+prashx1908@users.noreply.github.com>
2025-04-14 20:24:54 +0530

    File updated at 14/04/2025, 20:24:54

--- 217-ContainsDuplicate/217-ContainsDuplicate.cpp
+++ 217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -1,13 +1,14 @@
-// Last updated: 14/04/2025, 20:21:06
+// Last updated: 14/04/2025, 20:24:54
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if (nums[i] == nums[i+1]){
-                return true;
-            }
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        if(s==t){
+            return true;
         }
         return false;
+        
     }
 };
