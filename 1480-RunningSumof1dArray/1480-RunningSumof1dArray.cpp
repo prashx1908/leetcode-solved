5b9c45b98c77fa560d42fbb818ef22a155023088
Prashanth G <77740840+prashx1908@users.noreply.github.com>
2025-05-09 22:16:25 +0530

    File updated at 09/05/2025, 22:16:25

--- 1480-RunningSumof1dArray/1480-RunningSumof1dArray.cpp
+++ 1480-RunningSumof1dArray/1480-RunningSumof1dArray.cpp
@@ -1,19 +1,21 @@
-// Last updated: 25/03/2025, 20:14:33
+// Last updated: 09/05/2025, 22:16:25
 class Solution {
 public:
-    vector<int> runningSum(vector<int>& nums) {
+    int maximumWealth(vector<vector<int>>& accounts) {
 
-        for(int i =1;i<nums.size();i++){
-            nums[i] += nums[i-1];
-      
+        int maxwealth=0;
+
+        for (int i=0;i<accounts.size();++i){
+            int currentwealth=0;
+            for(int j=0;j<accounts[i].size();++j){
+                currentwealth += accounts[i][j];
+            }
+            if(currentwealth>maxwealth){
+                maxwealth=currentwealth;
+            }
         }
+        return maxwealth;
 
-        return nums;
-
-
-
-
-
-        
     }
+
 };
