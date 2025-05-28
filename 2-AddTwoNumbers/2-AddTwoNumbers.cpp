c320b458f87c55b0ac47b2a9d34b9113b642b396
Prashanth G <77740840+prashx1908@users.noreply.github.com>
2025-05-28 11:47:23 +0530

    File updated at 28/05/2025, 11:47:23

--- 2-AddTwoNumbers/2-AddTwoNumbers.cpp
+++ 2-AddTwoNumbers/2-AddTwoNumbers.cpp
@@ -1,14 +1,41 @@
-// Last updated: 10/05/2025, 22:00:28
+// Last updated: 28/05/2025, 11:47:23
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy= new ListNode(0);
+        ListNode* tail= dummy;
+        int carry=0;
 
-        for(int i=0;i<nums.size();++i){
-            if(nums[i]>=target){
-                return i;
-            }
+        while(l1 != nullptr || l2!= nullptr || carry !=0){
+            int digit1= (l1 != nullptr) ? l1->val :0;
+            int digit2= (l2 != nullptr) ? l2->val :0;
+            int sum = digit1+digit2+carry;
+            int digit= sum%10;
+            carry= sum/10;
+
+             ListNode* newnode= new ListNode(digit);
+             tail->next= newnode;
+             tail= tail->next;
+
+             l1= (l1 != nullptr) ? l1->next:nullptr;
+             l2= (l2 != nullptr) ? l2->next:nullptr;
+
+
+
         }
-     
-        return nums.size();
+        ListNode* result = dummy->next;
+        delete dummy;
+        return result;
+        
     }
 };
