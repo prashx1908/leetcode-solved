7cc523c6c7461255ef63855e422b7340b5535210
Prashanth G <77740840+prashx1908@users.noreply.github.com>
2025-05-28 14:52:41 +0530

    File updated at 28/05/2025, 14:52:41

--- 283-MoveZeroes/283-MoveZeroes.py
+++ 283-MoveZeroes/283-MoveZeroes.py
@@ -1,11 +1,22 @@
-# Last updated: 25/05/2025, 00:09:41
-class Solution(object):
-    def moveZeroes(self, nums):
-        left= 0
+# Last updated: 28/05/2025, 14:52:41
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph= defaultdict(dict)
 
-        for right in range(len(nums)):
-            if nums[right]!=0:
-                nums[left], nums[right]= nums[right], nums[left]
-                left +=1
-        return nums
+        for(u,v),val in zip(equations,values):
+            graph[u][u]= graph[v][v]=1
+            graph[u][v]= val
+            graph[v][u]= 1/val
+
+        for k in graph:
+            for i in graph[k]:
+                for j in graph[k]:
+                    graph[i][j]= graph[i][k]*graph[k][j] if i!=j else 1
+        ans=[]
+
+        for u,v in queries:
+            ans.append(graph[u].get(v,-1))
+        return ans
+
+
         
