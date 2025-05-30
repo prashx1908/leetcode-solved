e305b01130e39334786b41795b1f5e3c657d321a
Prashanth G <77740840+prashx1908@users.noreply.github.com>
2025-05-30 08:56:24 +0530

    Create 399-EvaluateDivision

---
+++ 399-EvaluateDivision/399-EvaluateDivision.py
@@ -0,0 +1,22 @@
+# Last updated: 30/05/2025, 08:56:23
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph= defaultdict(dict)
+
+        for(u,v),val in zip(equations,values):
+            graph[u][u]= graph[v][v]=1
+            graph[u][v]= val
+            graph[v][u]= 1/val
+
+        for k in graph:
+            for i in graph[k]:
+                for j in graph[k]:
+                    graph[i][j]= graph[i][k]*graph[k][j] if i!=j else 1
+        ans=[]
+
+        for u,v in queries:
+            ans.append(graph[u].get(v,-1))
+        return ans
+
+
+        
