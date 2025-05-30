a6e1135e3e50dbe178f36f8adf5d3c0b1accc005
Prashanth G <77740840+prashx1908@users.noreply.github.com>
2025-05-30 10:05:07 +0530

    File updated at 30/05/2025, 10:05:07

--- 58-LengthofLastWord/58-LengthofLastWord.cpp
+++ 58-LengthofLastWord/58-LengthofLastWord.cpp
@@ -1,16 +1,22 @@
-// Last updated: 30/05/2025, 09:58:49
+// Last updated: 30/05/2025, 10:05:07
 class Solution {
-public:
-    int lengthOfLastWord(string s) {
-        int end= s.length()-1;
+ public:
+  string addBinary(string a, string b) {
+    string ans;
+    int carry = 0;
+    int i = a.length() - 1;
+    int j = b.length() - 1;
 
-        while(end>=0 && s[end]==' '){
-            end--;
-        }
-        int start = end;
-        while(start>=0 && s[start] !=' '){
-            start--;
-        }
-        return end-start;
+    while (i >= 0 || j >= 0 || carry) {
+      if (i >= 0)
+        carry += a[i--] - '0';
+      if (j >= 0)
+        carry += b[j--] - '0';
+      ans += carry % 2 + '0';
+      carry /= 2;
     }
+
+    reverse(begin(ans), end(ans));
+    return ans;
+  }
 };
