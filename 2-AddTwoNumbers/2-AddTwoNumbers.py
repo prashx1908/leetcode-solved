01163a4794e35f4ca38271a2e3724bacfe3461dc
Prashanth G <77740840+prashx1908@users.noreply.github.com>
2025-06-02 12:06:00 +0530

    Create 2-AddTwoNumbers

---
+++ 2-AddTwoNumbers/2-AddTwoNumbers.py
@@ -0,0 +1,30 @@
+# Last updated: 02/06/2025, 12:06:00
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        INT_MAX = 2**31-1
+        INT_MIN= -2**31 
+        i=0
+        n= len(s)
+        while(i<n and s[i]==' '):
+            i+=1
+        if i==n:
+            return 0
+        sign =1
+        if(s[i]=='+'):
+            i+=1
+        elif(s[i]=='-'):
+            sign = -1
+            i+=1
+        
+        result=0
+        while i<n and s[i].isdigit():
+            digit = int(s[i])
+
+            if result > (INT_MAX - digit) // 10:
+                return INT_MAX if sign ==1 else INT_MIN
+            result= result*10+digit
+            i+=1
+        return sign*result
+        
+
+        
