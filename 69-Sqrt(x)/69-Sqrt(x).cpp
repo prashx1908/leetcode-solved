c941fdbd043e9f01b5cf8b028ec09bbaa794ef98
Prashanth G <77740840+prashx1908@users.noreply.github.com>
2025-06-03 10:08:08 +0530

    Create 69-Sqrt(x)

---
+++ 69-Sqrt(x)/69-Sqrt(x).cpp
@@ -0,0 +1,20 @@
+// Last updated: 03/06/2025, 10:08:07
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        return max(maxDepth(root->left), maxDepth(root->right))+1;
+        
+    }
+};
