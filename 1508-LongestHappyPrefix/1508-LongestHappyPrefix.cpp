ee0b4597b4df74bcb5bd84789ea60c4ddd793e9c
Prashanth G <77740840+prashx1908@users.noreply.github.com>
2025-06-03 09:38:07 +0530

    Create 1508-LongestHappyPrefix

---
+++ 1508-LongestHappyPrefix/1508-LongestHappyPrefix.cpp
@@ -0,0 +1,26 @@
+// Last updated: 03/06/2025, 09:38:07
+class Solution {
+public:
+    string longestPrefix(string s) {
+        vector<int> dp(s.size(), 0);
+        int len=0;
+        int i=1;
+        while(i<s.size()){
+            if(s[i]==s[len]){
+                dp[i]= ++len;
+                i++;
+            }
+            else {
+                if(len>0){
+                len= dp[len-1];
+            }
+            else{
+                dp[i]=0;
+                i++;
+            }
+            }
+        }
+        return s.substr(0,dp.back());
+        
+    }
+};
