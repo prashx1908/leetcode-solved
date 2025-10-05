11aecb8914972ad127384b134bb997d9b35e7d10
Prashanth G <77740840+prashx1908@users.noreply.github.com>
2025-10-05 22:11:50 +0530

    Create 803-CheapestFlightsWithinKStops

---
+++ 803-CheapestFlightsWithinKStops/803-CheapestFlightsWithinKStops.py
@@ -0,0 +1,15 @@
+# Last updated: 05/10/2025, 22:11:50
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prices=[float('inf')]*n
+        prices[src]=0
+        for i in range(k+1):
+            tmp= prices.copy()
+            for s,d,p in flights:
+                if prices[s]==float('inf'):
+                    continue
+                if p + prices[s] < tmp[d]:
+                    tmp[d]= p+prices[s]
+            prices=tmp
+        return -1 if prices[dst]==float('inf') else prices[dst]
+        
