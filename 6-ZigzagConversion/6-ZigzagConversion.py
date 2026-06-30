91b07cb1dfcee880004942f1d31c4ef394798d13
Prashanth G <77740840+prashx1908@users.noreply.github.com>
2026-06-30 22:12:54 +0530

    File updated at 30/06/2026, 22:12:53

--- 6-ZigzagConversion/6-ZigzagConversion.py
+++ 6-ZigzagConversion/6-ZigzagConversion.py
@@ -1,5 +1,24 @@
-# Last updated: 07/10/2025, 16:13:39
-class Solution:
-    def reverse(self, x: int) -> int:
-        ans = int(str(abs(x))[::-1]) * (-1 if x<0 else 1)
-        return ans if -2**31 <= ans < 2**31 else 0
+# Last updated: 30/06/2026, 22:12:53
+1class Solution:
+2    def convert(self, s: str, numRows: int) -> str:
+3
+4        if numRows == 1 or numRows >= len(s):
+5            return s
+6
+7        rows= [""] * numRows
+8        current=0
+9        going=True
+10
+11        for ch in s:
+12            rows[current] += ch
+13            if current ==0:
+14                going = True
+15            elif current == numRows -1:
+16                going = False
+17        
+18            if going:
+19                current +=1
+20            else:
+21                current -=1
+22        return "".join(rows)
+23        
